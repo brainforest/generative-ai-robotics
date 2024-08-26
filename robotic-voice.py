648d0305748803723c370fa742a124871da28f2e
GokcerBelgusen <gokcerb@gmail.com>
2024-08-26 12:33:07 +0300

    Commit message

--- robotic-voice.py
+++ robotic-voice.py
@@ -2,9 +2,17 @@ import pyaudio
 import speech_recognition as sr
 import pyttsx3
 import random
+from pixels import Pixels
+import time
+
+# Pixel animations during record, speak and think
+pixels = Pixels()
 
 # Initialize recognizer and text-to-speech engine
 engine = pyttsx3.init()
+
+pixels.wakeup()
+time.sleep(3)
 
 # Greetings to choose from
 greetings = ["Hello, how can I assist you?", "Hi there! What do you need?", "Hey! How can I help you?","Yes, master I listen you"]
@@ -17,8 +25,14 @@ print(f"Current speech rate: {current_rate}")
 slow_rate = current_rate - 100  # Decrease by 50, you can adjust this value
 engine.setProperty('rate', slow_rate)
 
-engine.say(random.choice(greetings))
-engine.runAndWait()
+pixels.speak()
+
+#engine.say(random.choice(greetings))
+#engine.runAndWait()
 
 engine.say("A black hole is a region of spacetime where gravity is so strong that nothing, not even light and other electromagnetic waves, is capable of possessing enough energy to escape it. Einstein's theory of general relativity predicts that a sufficiently compact mass can deform spacetime to form a black hole.")
 engine.runAndWait()
+
+
+pixels.off()
+time.sleep(3)
